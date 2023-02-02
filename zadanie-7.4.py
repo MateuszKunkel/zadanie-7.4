64d60102f09dca6ef15a6ac80784ed4357e7f06b
Mateusz Kunkel <matiqk@wp.pl>
2023-02-02 19:27:30 +0100

    code version ready to deliver

--- zadanie-7.4.py
+++ zadanie-7.4.py
@@ -64,24 +64,37 @@ class Serial(Film):
 
 
 def get_movies():
-    by_movies = sorted(list, key=lambda Film: Film.tytul)
-    by_movies.sort()
-    print(*by_movies, sep="\n")
+    
+    movies_list = []
+
+    for i in list:
+        if isinstance(i, Serial) == False:
+            movies_list.append(i)
+        
+    by_movies = sorted(movies_list, key=lambda Film: Film.tytul)
+    return by_movies
 
 
 def get_series():
-    by_series = sorted(list, key=lambda Serial: Serial.tytul)
-    by_series.sort()
-    print(*by_series, sep="\n")
+
+    series_list = []
+
+    for i in list:
+        if isinstance(i, Serial):
+            series_list.append(i)
+    
+    by_series = sorted(series_list, key=lambda Serial: Serial.tytul)
+    return by_series
 
 
 def search(title):
+    is_found = 0
     for element in list:
         if element.tytul == title:
+            is_found += 1
             print("Posiadamy to arcydzieło!")
-        else:
-            print("Brak wyników, sorki.")
-
+    if is_found == 0:
+        print("Brak wyników wyszukiwania")
 
 def generate_views():
     unnecessary_long_random_number_generator = random.randint(1, len(list))
@@ -114,25 +127,25 @@ def top_titles(amount, content_type):
     if content_type == "Film":
 
         by_views_limited = []
-        by_views = sorted(list, reverse=True, key=lambda video: video._liczba)
+        by_views = sorted(get_movies(), reverse=True, key=lambda video: video._liczba)
         for i in range(chosen_top):
             by_views_limited.append(by_views[i])
+        print("Najlepsze pod względem wyświetleń filmy to:")
         print(*by_views_limited, sep="\n")
 
-    elif content_type == "Serial":
+    if content_type == "Serial":
 
         by_views_limited = []
-        by_views = sorted(list, reverse=True, key=lambda video: video._liczba)
+        by_views = sorted(get_series(), reverse=True, key=lambda video: video._liczba)
         for i in range(chosen_top):
             by_views_limited.append(by_views[i])
+        print("Najlepsze pod względem wyświetleń seriale to:")
         print(*by_views_limited, sep="\n")
-
-    else:
-        print("Error, invalid . Ya sure there is no typo Bro?")
 
 
 def the_world_is_but_a_stage_and_the_stage_is_the_world_of_entertainment():
 
+    generic_library_list = []
     generic_library_quantity = 20
 
     while generic_library_quantity > 0:
@@ -157,7 +170,7 @@ def the_world_is_but_a_stage_and_the_stage_is_the_world_of_entertainment():
                     ],
                 ),
             )
-            list.append(video)
+            generic_library_list.append(video)
         else:
             video = Serial(
                 tytul=fake.sentence(nb_words=3, variable_nb_words=False),
@@ -178,16 +191,18 @@ def the_world_is_but_a_stage_and_the_stage_is_the_world_of_entertainment():
                 sezony=random.randint(1, 10),
                 odcinki_na_sezon=random.randrange(10, 24, 2),
             )
-            list.append(video)
+            generic_library_list.append(video)
 
         generic_library_quantity -= 1
+
+    return generic_library_list
 
 
 if __name__ == "__main__":
 
-    list = []
+    separator = "-------------------------------------------------------------"
 
-    the_world_is_but_a_stage_and_the_stage_is_the_world_of_entertainment()
+    list = the_world_is_but_a_stage_and_the_stage_is_the_world_of_entertainment()
     start_variable = 10
     while start_variable > 0:
         we_need_more_views()
@@ -200,33 +215,78 @@ if __name__ == "__main__":
     thatday = today.strftime("%d.%m.%Y")
     print(f"Dzisiejsze ({thatday}) top 3 to:")
     top_3_titles()
-    print("-------------------------------------------------------------")
+    print(separator)
 
+    print(
+        "Czy chcesz zobaczyć listę wszystkich posiadanych filmów i seriali?"
+    )
     choice1 = input(
-        "Czy chcesz zobaczyć listę wszystkich posiadanych filmów i seriali? (jeśli *nie*, następne zapytanie dotyczyć będzie toplist filmów i seriali) ( Y / N ):"
+        "( Y / N ):"
     )
 
     if choice1 == "Y":
         print(*list, sep="\n")
+    print(separator)
 
-    # choice2 = input("Czy chcesz zobaczyć toplistę posiadanych filmów? (następne zapytanie dotyczyć będzie ilości) ( Y / N ):")
-    # if choice2 == "Y":
-    #    choice2_2 = int(input("Ile dzieł ma być w topliście?:"))
-    #    top_titles(choice2_2, "Film")
-
-    # choice3 = input("Czy chcesz zobaczyć toplistę posiadanych seriali? (następne zapytanie dotyczyć będzie ilości) ( Y / N ):")
-    # if choice3 == "Y":
-    #    choice3_2 = int(input("Ile dzieł ma być w topliście?:"))
-    #    top_titles(choice3_2, "Serial")
+    print(
+        "Czy chcesz zobaczyć toplistę posiadanych filmów lub seriali?\n(następne zapytanie dotyczyć będzie wyboru serial/film)"
+    )
+    choice2 = input("( Y / N ):")
+    if choice2 == "Y":
+        print("Wybierz S jeśli seriale, F jeśli filmy\n(następne zapytanie dotyczyć będzie ilości obiektów na liście)")
+        choice2_2 = input("( S / F ):")
+        if choice2_2 == "S":
+            print("wszystkie posiadane seriale ułożone alfabetycznie to:")
+            print(*get_series(), sep="\n")
+            print(separator)
+            choice2_3 = int(input("Ile seriali ma być w topliście?:"))
+            top_titles(choice2_3, "Serial")
+        elif choice2_2 == "F":
+            print("wszystkie posiadane filmy ułożone alfabetycznie to:")
+            print(*get_movies(), sep="\n")
+            print(separator)
+            choice2_3 = int(input("Ile filmów ma być w topliście?:"))
+            top_titles(choice2_3, "Film")
+        else:
+            print("zła litera, lecimy dalej")
+    print(separator)
 
     task10 = input(
-        "Czy chcesz dodatkowo podbić ranking wyświetleń? (polecenie nr. 10) ( Y / N ):"
+        "Czy chcesz dodatkowo podbić ranking wyświetleń? (polecenie nr. 10)\n( Y / N ):"
     )
     if task10 == "Y":
         we_need_more_views()
+    print(separator)
 
-    # print("Czy chcesz wyszukać któreś konkretne wspaniałe dzieło sztuki filmowej? (polecenie nr. 8) ( Y / N )")
+    task8 = input(
+        "Czy chcesz wyszukać któreś konkretne wspaniałe dzieło sztuki filmowej? (polecenie nr. 8)\n( Y / N ):"
+    )
+    if task8 == "Y":
+        print("Upewnij się, że dobrze przepiszesz któryś z wyżej wypisanych tytułów i ZAKOŃCZYSZ GO KROPKĄ!")
+        print("Lub nie, kim ja jestem by ci kazać, meh.")
+        search(
+            input("O obecność której taśmy chcesz zapytać:")
+        )
+    print(separator)
 
-    # print("Czy chcesz odtworzyć któreś z wyżej wypisanych arcydzieł kinematografii? (polecenie nr. 3) ( Y / N )")
+    task3 = input(
+        "Czy chcesz odtworzyć któreś z wyżej wypisanych arcydzieł kinematografii? (polecenie nr. 3)\n( Y / N ):"
+    )
+    if task3 == "Y":
+        print("Wybierz z listy wszystkich materiałów który z kolei chcesz odtworzyć:")
+        print(*list, sep="\n")
+        one_more_choice = int(input())
+        print(separator)
+        print("Zanim zaczniej oglądać najważniejsze pytanie:")
+        the_most_inportant_choice = input("Popcorn i orzeszki do seansu? ( Y / N )")
+        if the_most_inportant_choice == "Y":
+            print(".______     ______   .______     ______   ______   .______      .__   __.  __  ")
+            print("|   _  \   /  __  \  |   _  \   /      | /  __  \  |   _  \     |  \ |  | |  | ")
+            print("|  |_)  | |  |  |  | |  |_)  | |  ,----'|  |  |  | |  |_)  |    |   \|  | |  | ")
+            print("|   ___/  |  |  |  | |   ___/  |  |     |  |  |  | |      /     |  . `  | |  | ")
+            print("|  |      |  `--'  | |  |      |  `----.|  `--'  | |  |\  \----.|  |\   | |__| ")
+            print("| _|       \______/  | _|       \______| \______/  | _| `._____||__| \__| (__) ")
 
-    # print("Popcorn i orzeszki do seansu? ( Y / N )")
+        list[one_more_choice - 1].play()
+        print("Brawo, dodano wyświetlenie.")
+
